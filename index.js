4510b95d1f92198a394dce163f1e735a2c993a18
sadaab2784 <106314415+sadaab2784@users.noreply.github.com>
2022-05-27 13:50:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// function for Displaying time
 const timer = document.getElementById("watch_container");
 var hour = 0;
 var minute = 0;
 var second = 0;
 var stoptime = true;
 
+// function for start time and calling the timerCycle function
 function startTimer(){
     if(stoptime == true){
         stoptime = false;
@@ -11,12 +13,14 @@ function startTimer(){
     }
 }
 
+// function for Stop the time 
 function stopTimer(){
     if(stoptime == false){
         stoptime = true;
     }
 }
 
+// function for Reset the time
 function resetTimer(){
     timer.innerHTML = "00:00:00";
     stoptime = true;
@@ -27,30 +31,36 @@ function resetTimer(){
 
 function timerCycle(){
     if(stoptime  == false){
-        second = parseInt(second);
+        second = parseInt(second);    // Here the parseInt convert the second, minute & hour into Integer
         minute = parseInt(minute);
         hour = parseInt(hour);
 
         second = second + 1;
 
+        // here the condition, if second is equal to 60 then it converted into minute
         if(second == 60){
             minute = minute + 1; 
             second = 0;       
         }
+        
+        // same as second, if the minute is equal to 60 then it converted into hour
         if(minute == 60){
             hour = hour + 1;
             minute = 0;
             second = 0;
         }
         
+        // here the condition, if second is lesser than 10 then concatenate "0" in the second
         if(second < 10){
             second = "0" + second;
         }
 
+        // same as second, if minute is lesser than 10 then concatenate "0" in the minute
         if(minute < 10){
             minute = "0" + minute;
         }
-
+        
+        // same as minute, if hour is lesser than 10 then concatenate "0" in the hour
         if(hour < 10){
             hour = "0" + hour;
         }
